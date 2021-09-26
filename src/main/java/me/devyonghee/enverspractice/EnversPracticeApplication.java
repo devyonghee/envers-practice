f153f270a01c9574c791305cbf2e932187f1e255
devyonghee <devyonghee@gmail.com>
2021-09-26 23:30:11 +0900

    first commit

---
+++ src/main/java/me/devyonghee/enverspractice/EnversPracticeApplication.java
@@ -0,0 +1,13 @@
+package me.devyonghee.enverspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnversPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnversPracticeApplication.class, args);
+    }
+
+}
