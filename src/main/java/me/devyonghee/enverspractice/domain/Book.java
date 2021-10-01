f9b83b3e5ca2d1e47c6458d8977fbb0e94f41056
devyonghee <devyonghee@gmail.com>
2021-10-02 00:06:52 +0900

    add book audited

--- src/main/java/me/devyonghee/enverspractice/domain/Book.java
+++ src/main/java/me/devyonghee/enverspractice/domain/Book.java
@@ -5,9 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import org.hibernate.envers.Audited;
 
 @Entity
+@Audited
 public class Book {
 
     @Id
